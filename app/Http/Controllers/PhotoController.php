6ebf4c556a75f6abc68a488a7731535e3e53f9b5
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-11-20 18:46:35 +0600

    Photo Controller updated for CRUD

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,82 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PhotoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // public function index()
-    // {
-    //     //
-    // }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create($gallery_id)
     {
-        //
+        return view('photo.create', compact('gallery_id'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $gallery_id     = $request->input('gallery_id');
+        $title          = $request->input('title');
+        $description    = $request->input('description');
+        $location       = $request->input('location');
+        $image          = $request->file('image');
+        $owner_id       = 1;
+
+        // Check image upload
+        
+        if($image){
+            $image_filename = $image->getClientOriginalName();
+            $image->move(public_path('images'), $image_filename);
+        }else{
+            $image_filename = 'noimage.jpg';
+        }
+
+        // Insert Photo
+        DB::table('photos')->insert(
+            [
+                'title'         => $title,
+                'description'   => $description,
+                'location'      => $location,
+                'gallery_id'    => $gallery_id,
+                'image'         => $image_filename,
+                'owner_id'      => $owner_id
+            ]
+        );
+
+        \Session::flash('message', 'Photo Added');
+
+        return \Redirect::route('gallery.show', array('id' => $gallery_id));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $photo = DB::table('photos')->where('id', $id)->first();
+        return view('photo/show', compact('photo'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
