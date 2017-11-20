724e1a1b4f20b3e885e9d5a266a1fa2184a926f8
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-11-20 17:51:26 +0600

    all galleries shown

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -10,9 +10,10 @@
       @foreach($galleries as $gallery)
         <div class="col-lg-3 col-md-4 col-xs-6">
           <a href="gallery/show/{{ $gallery->id }}" >
-            <img class="img-fluid img-thumbnail" src="/images/{{ $gallery->cover_image }}" alt="">
+            <img class="img-fluid img-thumbnail" src="/images/{{ $gallery->cover_image }}" style="height: 150px; width: 300px;">
           </a>
           <div class="d-block mb-4 h-100">
+            <br>
             <h5>{{ $gallery->name }}</h5>
             <p>{{ $gallery->description }}</p>
           </div>
